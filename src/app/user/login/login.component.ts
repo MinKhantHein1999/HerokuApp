8049d7ea3cba4e4555a1f3b831fd281dee3dacf2
Min Khant Hein <minkhanthein.15-16@ucsmub.edu.mm>
2020-10-20 20:57:03 +0630

    second register

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  hide = true;
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.minLength(6)])
+    ),
+  });
+  messageClass: string;
+  message: any;
 
-  ngOnInit(): void {
+  get Email() {
+    return this.loginForm.get('email');
   }
 
+  get Password() {
+    return this.loginForm.get('password');
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  login() {}
 }
